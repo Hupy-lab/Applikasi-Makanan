e70cdfde4c1b66bcb6689d888f92060506efdcb3
Irfan Dani Syahputra <irfan_danisyahputra@yahoo.com>
2019-10-21 10:49:21 -0700

    MainActivity.java

--- app/src/main/java/com/example/menumakanan/MainActivity.java
+++ app/src/main/java/com/example/menumakanan/MainActivity.java
@@ -29,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
     void addData(){
         makananArrayList = new ArrayList<>();
-        makananArrayList.add(new makanan("Bubur Sum Sum","Bubur kanji yang lembut di padukan gula aren yang legit", "9000", "gambar1"));
-        makananArrayList.add(new makanan("Ayam Geprek","Ayam goreng tepung yang dicampur dengan sambal yang nikmat", "10000", "gambar2"));
-        makananArrayList.add(new makanan("Roti bakar","Roti tawar dibakar dan diberikan parutan keju dan lelehan coklat", "14000", "gambar3"));
+        makananArrayList.add(new makanan("Ayam Geprek","Ayam goreng tepung yang dicampur dengan sambal yang nikmat", "9000", "gambar1"));
+        makananArrayList.add(new makanan("Ayam Bakar","Ayam yang di bakar dengan di beri kecap yg nikmat", "10000", "gambar2"));
+        makananArrayList.add(new makanan("Nasi goreng","Nasi yang di goreng dan diberi bumbu rempah yang nikmat", "14000", "gambar3"));
         makananArrayList.add(new makanan("Sate Ayam","Potongan ayam yang ditusuk dan dibakar hingga sempurna", "22000", "gambar4"));
     }
 }
